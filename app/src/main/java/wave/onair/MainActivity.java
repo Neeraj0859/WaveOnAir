16b2bafb27ff068a090cd959969bcc18eb1933af
Neeraj0859 <neerajdhameliya@gmail.com>
2018-08-06 16:49:06 +0530

    basic Structure partially done

--- app/src/main/java/wave/onair/MainActivity.java
+++ app/src/main/java/wave/onair/MainActivity.java
@@ -1,13 +1,44 @@
 package wave.onair;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar app_bar;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initiate_ids();
     }
+
+
+    private void initiate_ids() {
+        app_bar = findViewById(R.id.app_bar);
+        setSupportActionBar(app_bar);
+
+        drawer = findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, app_bar, R.string.drawer_is_open,
+                R.string.drawer_is_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(Gravity.START)) {
+            drawer.closeDrawer(Gravity.START);
+        } else
+            super.onBackPressed();
+    }
+
+
 }
